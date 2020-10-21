dbc667c56bc88e0c76dfb38a0519ebc107007f74
cassidoo <cassidoo@gmail.com>
2020-10-21 16:53:19 -0500

    Capture form submission and push to Hasura

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,7 +1,37 @@
+const fetch = require('node-fetch');
+
 exports.handler = async (event) => {
-  console.log(event);
+  let { name, gender, smell } = JSON.parse(event.body).payload.data;
+
+  await addCharacter(name, gender, smell);
+
   return {
     statusCode: 200,
-    body: 'okay!',
+    body: `Character added: ${name}, ${gender}, ${smell}`,
   };
+};
+
+const addCharacter = async (name, pronouns, smell) => {
+  const idCreated = await fetch(process.env.HASURA_URL, {
+    method: 'POST',
+    headers: {
+      'X-Hasura-Admin-Secret': process.env.HASURA_ADMIN,
+    },
+    body: JSON.stringify({
+      query: `
+      mutation addCharacter($name: String!, $smell: String!, $pronouns: String!) {
+        insert_characters_one(object: {name: $name, pronouns: $pronouns, smell: $smell}) {
+          id
+        }
+      }
+      `,
+      variables: {
+        name,
+        pronouns,
+        smell,
+      },
+    }),
+  }).then((res) => res.json());
+
+  return idCreated;
 };
