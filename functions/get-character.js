6eef04a60d2dc52c1100dd281116a46c34aa2755
cassidoo <cassidoo@gmail.com>
2020-10-21 17:04:13 -0500

    Add get character function

---
+++ functions/get-character.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+
+exports.handler = async () => {
+  let characters = await getCharacters();
+
+  let randomCharacter =
+    characters[Math.round(Math.random() * (characters.length - 1))];
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(randomCharacter),
+  };
+};
+
+const getCharacters = async () => {
+  const response = await fetch(process.env.HASURA_URL, {
+    method: 'POST',
+    headers: {
+      'X-Hasura-Admin-Secret': process.env.HASURA_ADMIN,
+    },
+    body: JSON.stringify({
+      query: `
+        query QueryCharacters {
+            characters {
+              name
+              pronouns
+              smell
+            }
+          }
+      `,
+      variables: {},
+    }),
+  }).then((res) => res.json());
+
+  return response.data.characters;
+};
