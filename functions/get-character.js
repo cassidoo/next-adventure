aaae6eb65c14396d79ca8861bb3a97317cb3eae7
cassidoo <cassidoo@gmail.com>
2020-10-21 18:07:30 -0500

    Add access headers to endpoint

--- functions/get-character.js
+++ functions/get-character.js
@@ -8,6 +8,11 @@ exports.handler = async () => {
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+    },
     body: JSON.stringify(randomCharacter),
   };
 };
